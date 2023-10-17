e9d7e8b8a2ad14f134c8ece333e9583c8a9d2c91
ts <dkelak10@gmail.com>
2023-10-17 16:10:12 +0900

    장고 데이터 전달 방식과 통신 방식에 대해 실습함.

--- ch99/polls/views.py
+++ ch99/polls/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render, HttpResponse
+from django.views.decorators.csrf import csrf_exempt    
 import random
 
+nextId = 4
 topics = [
     {'id' : 1, 'title': '전자통신컴퓨터공학부', 'body': '전자통신컴퓨터공학부에 오신걸 환영합니다.'},
     {'id' : 2, 'title': '간호학과', 'body': '간호학과에 오신걸 환영합니다.'},
-    {'id' : 3, 'title': '유아교육학과', 'body': '유아교육학과에 오신걸 환영합니다.'},
-    {'id' : 4, 'title': '기계자동차공학부', 'body': '기계자동차공학부에 오신걸 환영합니다.'},
-    {'id' : 5, 'title': '물리치료학과', 'body': '물리치료학과에 오신걸 환영합니다.'},
-    {'id' : 6, 'title': '특수건설기계학과', 'body': '특수건설기계학과에 오신걸 환영합니다.'}
+    {'id' : 3, 'title': '유아교육학과', 'body': '유아교육학과에 오신걸 환영합니다.'}
     ]
 
-def HtML_Template(articletag):
+def HtML_Template(articleTag):
     global topics
     ol = ''
     for topic in topics:
@@ -22,17 +21,38 @@ def HtML_Template(articletag):
             <ol>
                 {ol}
             </ol>
-            {articletag}
+            {articleTag}
+            <ul>
+                <li><a href = "/create/">create</a></li>
+            <ul>
         </body>
         </html>
     '''
 def index(request):
-    articletag = '''<h2>Welcome</h2>
+    article = '''<h2>Welcome</h2>
     안녕하세요. 장고입니다.'''
-    return HttpResponse(HtML_Template(articletag))
+    return HttpResponse(HtML_Template(article))
 
-def creat(request):
-    return HttpResponse('안녕하세요 creat 홈페이지에 오신걸 환영합니다')
+@csrf_exempt
+def create(request):
+    global nextId    
+    if request.method == 'GET':
+        article = '''
+            <form action = "/create/" method = "POST">
+                <p><input type = "text" name = "title" placeholder = "title"></p>
+                <p><textarea name = "body" placeholder = "body"></textarea name.</p>
+                <p><input type = "submit"></p> 
+            </form>
+        '''
+        return HttpResponse(HtML_Template(article))
+    elif request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body']
+        newTopic = {"id": nextId, "title": title, "body": body}
+        topics.append = (newTopic)
+        nextId = nextId + 1
+        print(request.POST)
+        return HttpResponse(HtML_Template('aaa'))
 
 def read(request, id):
     global topics
